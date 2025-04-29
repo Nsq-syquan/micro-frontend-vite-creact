ec1ed4f6cd9c0ccce9a8080b4395da4eb5486db7
Nguyễn Quân <nguyenquan@188277-Quan-2.local>
2025-04-29 15:16:18 +0700

    test case host expose component

--- host_app/src/components/AButton/index.jsx
+++ host_app/src/components/AButton/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const AButton = () => {
+const AButton = ({ title = "A Button Host", onClick }) => {
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      A Button
+    <button onClick={onClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      {title}
     </button>
   )
 }
--- remote_one/src/features/Home/index.jsx
+++ remote_one/src/features/Home/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 const AButton = React.lazy(() => import('host/AButton'))
 
 const Home = () => {
+  const navigate = useNavigate()
+
   return (
     <div>
       Home MFE1
-      <AButton />
+      <AButton title="Trở về trang chủ host" onClick={()=> navigate("/")}   />
     </div>
   )
 }
